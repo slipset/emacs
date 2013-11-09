a015a7e9eda1e39625c433c028f1b343533d670a
Erik Assum <erik.assum@joint.no>
2013-11-09 14:36:04 +0100

    also added setup at work

--- init.el
+++ init.el
@@ -1,12 +1,21 @@
 (setq init-home-dir "~/.emacs.d/")
 
 
-
 (require 'ensure-packages)
 (setq ensure-packages
-      '(projectile))
+      '(projectile simple-httpd flymake-jslint flymake-cursor git uniquify))
 
 (ensure-packages-install-missing)
+
+(load-theme 'tango-dark)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq-default cursor-type 'bar)
+(set-face-attribute 'default nil :height 110)
+
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 
 (require 'auto-complete-config)
 
@@ -38,3 +47,69 @@
 (setq mac-command-modifier 'meta)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+
+(add-to-list 'load-path "~/node/lintnode")
+(require 'flymake-jslint)
+;; Make sure we can find the lintnode executable
+(setq lintnode-location "~/node/lintnode")
+;; JSLint can be... opinionated
+(setq lintnode-jslint-set "sloppy:true,todo:true")
+(setq lintnode-jslint-excludes (list 'nomen 'plusplus 'onevar 'white ))
+;; Start the server when we first open a js file and start checking
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (flymake-mode t)
+			(setq tab-width 4)))
+
+;; (add-hook 'js-mode-hook 'smart-tabs-mode-enable)
+
+(require 'flymake-cursor)
+
+(add-to-list 'load-path "/usr/share/emacs24/site-lisp/git/")
+(require 'git)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(httpd-port 8848)
+ '(js-enabled-frameworks (quote (javascript extjs)))
+ '(js2-global-externs (quote ("Ext")))
+ '(lintnode-location "~/node/lintnode")
+ '(lintnode-node-program "nodejs"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq indent-tabs-mode t) ;; use tabs for indentation
+
+(autoload 'smart-tabs-mode "smart-tabs-mode"
+   "Intelligently indent with tabs, align with spaces!")
+(autoload 'smart-tabs-mode-enable "smart-tabs-mode")
+(autoload 'smart-tabs-advice "smart-tabs-mode")
+(autoload 'smart-tabs-insinuate "smart-tabs-mode")
+
+(smart-tabs-advice js-indent-line js-basic-offset)
+(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+                        'ruby 'nxml)
+(setq tab-width 4)        ;; set your desired tab width
+(require 'projectile)
+(add-hook 'js2-mode-hook 'projectile-on)
+
+
+(global-auto-revert-mode 1)
+
+(set-face-attribute 'default nil :height 80)
+	
+(modify-coding-system-alist 'file "\\.js\\'" 'utf-8-dos)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(require 'simple-httpd)
+(setq httpd-root "~/workspace/")
+(httpd-start)
