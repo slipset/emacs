1948f0d259392bfe570df90b7ca35c79bfb930e7
Erik Assum <erik@assum.net>
2017-10-24 22:09:15 +0200

    Does this have a purpose?

--- elisp/slipset-purpose.el
+++ elisp/slipset-purpose.el
@@ -1,5 +1,12 @@
 (require 'window-purpose)
 (require 'window-purpose-x)
 (purpose-x-magit-multi-on)
+(purpose-mode)
 
+(add-to-list 'purpose-user-mode-purposes '(clojure-mode . src))
+(add-to-list 'purpose-user-mode-purposes '(emacs-lisp-mode . src))
+(add-to-list 'purpose-user-mode-purposes '(cider-repl-mode . clj-repl))
+(add-to-list 'purpose-user-mode-purposes '(eshell-mode . shells))
+(add-to-list 'purpose-user-mode-purposes '(magit-status-mode . shells))
+(purpose-compile-user-configuration)
 (provide 'slipset-purpose)
